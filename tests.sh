3aebde87d865886f7269da951cd41947fd6a8426
m0na <monaw@princeton.edu>
2020-12-01 23:13:42 -0500

    poorly written tests

--- tests.sh
+++ tests.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
-set -e
 
-docker-compose build
+docker-compose build > /dev/null
 docker-compose up -d
 
-echo "GET server index"
-docker exec -it wg_client curl --http3 https://server:4433
-if [ $? -eq 0 ]; then
-	echo "... SUCCESS\n"
-else
-	echo "... FAILED\n"
-fi
+function dotest {
+	out="$($@)"
+	if [ $? -eq 0 ]; then
+		echo "  ... SUCCESS"
+	else
+		echo "$out"
+		echo "  ... FAILED"
+	fi
+}
+
+docker exec -it wg_client  wg-quick up wg1_client > /dev/null
+timeout 5 docker exec wg1 sh -c "tcpdump -U -c 5 udp > tmp.out" &
+sleep 1
+echo "###GET server index via wg1"
+dotest docker exec -it wg_client curl --http3 https://10.3.0.4:4433
+echo "### wg1 sees UDP traffic"
+dotest docker exec -it wg1 grep UDP tmp.out
+docker exec -it wg1 rm tmp.out
+docker exec -it wg_client  wg-quick down wg1_client > /dev/null
+
+# changing from wg1 to wg2
+docker exec -it wg_client  wg-quick up wg2_client > /dev/null
+timeout 5 docker exec wg2 sh -c "tcpdump -U -c 5 udp > tmp.out" &
+sleep 1
+echo "###GET server index via wg2"
+dotest docker exec -it wg_client curl --http3 https://10.3.0.4:4433
+echo "###wg2 sees UDP traffic"
+dotest docker exec -it wg2 grep UDP tmp.out
+docker exec -it wg2 rm tmp.out
+docker exec -it wg_client  wg-quick up wg1_client > /dev/null
 
 docker-compose down
+
