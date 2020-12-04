b7b12917b628078e099eb72a830aa0d51874f616
mona <monaxwang@gmail.com>
2020-12-04 15:50:42 -0800

    commit working copy of chrome-quic

--- chromium-quic/server.sh
+++ chromium-quic/server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/src/out/Debug/quic_server --quic_response_cache_dir=/site/example.com --certificate_file=/site/tls/leaf_cert.pem --key_file=/site/tls/leaf_cert.pkcs8
+/src/out/Debug/quic_server --quic_response_cache_dir=/app/site/example.com --certificate_file=/app/site/tls/leaf_cert.pem --key_file=/app/site/tls/leaf_cert.pkcs8 --v=1
