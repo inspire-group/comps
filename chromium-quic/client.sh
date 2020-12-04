7f5c95c7972a8250b454db69e783e939f974e56e
mona <monaxwang@gmail.com>
2020-12-04 13:58:07 -0800

    Add chrome docker images

---
+++ chromium-quic/client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/src/out/Debug/quic_client --host=server --port=6121 --disable_certificate_verification https://www.example.org
---
+++ chromium-quic/server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/src/out/Debug/quic_server --quic_response_cache_dir=/site/example.com --certificate_file=/site/tls/leaf_cert.pem --key_file=/site/tls/leaf_cert.pkcs8
