0e7c1bdd9b804680e5119bcff4db767148709221
mona <monaw@princeton.edu>
2020-12-08 20:51:25 -0500

    remote experiment checkpoint

--- wg_client/setup.sh
+++ wg_client/setup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+set -x
 ip link add wg1 type wireguard
-wg setconf wg1 /etc/wireguard/wg1_client.conf
+wg setconf wg1 /etc/wireguard/wg1.conf
 ip link set mtu 1420 up dev wg1
-ip -4 address add 10.0.0.2/32 dev wg1
+ip -4 address add 10.0.0.1/32 dev wg1
 
 wg set wg1 fwmark 51820
 ip -4 route add 0.0.0.0/0 dev wg1 table 51820
 ip -4 rule add not fwmark 51820 table 51820
 
 ip link add wg2 type wireguard
-wg setconf wg2 /etc/wireguard/wg2_client.conf
+wg setconf wg2 /etc/wireguard/wg2.conf
 ip link set mtu 1420 up dev wg2
-ip -4 address add 10.0.0.3/32 dev wg2
+ip -4 address add 10.0.1.1/32 dev wg2
 
 wg set wg2 fwmark 51821
 # ip -4 route add 0.0.0.0/0 dev wg2 table 51821
 ip -4 rule add not fwmark 51821 table 51821
 
 ip -4 rule add table main suppress_prefixlength 0
-# sysctl -q net.ipv4.conf.all.src_valid_mark=1
+sysctl -q net.ipv4.conf.all.src_valid_mark=1
